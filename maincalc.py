135f28ecafceb692b10c62bbdb9e6f4cd8523dd5
Nirmal Maharjan <nirmalmhjr@gmail.com>
2023-03-07 22:23:35 +0545

    Main file done

--- maincalc.py
+++ maincalc.py
@@ -1,1 +1,16 @@
 #This is by Nirmal Maharjan
+
+import divide
+import multiply
+import subtraction
+import sum
+
+a = 100
+b = 50
+
+total_sum =  sum.add(a,b)
+total_sub = subtraction.sub(a,b)
+total_mul = multiply.mul(a,b)
+total_div = divide.div(a,b) 
+
+print(f"Total sum is {total_sum}. Total subtraction is {total_sub}.Total multiplicatio is {total_mul}. Total division is {total_div} ")
